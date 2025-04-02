dc09ea514b56a6bfcf66321463f7f41d6345b38f
JaquePaula <jaquemarques.santos@gmail.com>
2025-04-02 16:27:45 -0300

    video aula 1

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -1,24 +1,57 @@
-﻿namespace MauiAppTempoAgora;
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
 
-public partial class MainPage : ContentPage
+namespace MauiAppTempoAgora
 {
-	int count = 0;
-
-	public MainPage()
+	public partial class MainPage : ContentPage
 	{
-		InitializeComponent();
+		int count = 0;
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private async void Button_Clicked(object sender, EventArgs e)
+		{
+			try
+			{
+				if (!string.IsNullOrEmpty(txt_cidade.Text))
+				{
+					Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
+
+					if (t != null)
+					{
+						string dados_previsao = "";
+
+						dados_previsao = $"Latitude: {t.lat} \n" +
+														 $"Longitude: {t.lon} \n" +
+														 $"Nascer do Sol: {t.sunrise} \n" +
+														 $"Por do Sol: {t.sunset} \n" +
+														 $"Temp Máx: {t.temp_max} \n" +
+														 $"Temp Min: {t.temp_min} \n";
+
+						lbl_res.Text = dados_previsao;
+
+					}
+					else
+					{
+
+						lbl_res.Text = "Sem dados de Previsão";
+					}
+
+				}
+				else
+				{
+					lbl_res.Text = "Preencha a cidade.";
+				}
+
+			}
+			catch (Exception ex)
+			{
+				await DisplayAlert("Ops", ex.Message, "OK");
+			}
+		}
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
 }
-
